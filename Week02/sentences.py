adc4214f27192d1d5f7f95fdcd3455b135a9ddbf
jdalmeria-tech <alm24008@byui.edu>
2024-09-28 09:25:10 +0800

    added prepositional phrases and adverb to the program :)
    final output for week2 project

--- Week02/sentences.py
+++ Week02/sentences.py
@@ -114,9 +114,63 @@ def make_sentence(quantity, tense):
   determiner = get_determiner(quantity)
   noun = get_noun(quantity)
   verb = get_verb(quantity, tense)
+  phrase1 = get_prepositional_phrase(quantity)
+  phrase2 = get_prepositional_phrase(quantity) # exceeding requirements no.1
+  adverb = get_adverb()
 
-  sentence = f"{determiner} {noun} {verb}."
+  sentence = f"{phrase1}, {determiner} {noun} {verb} {adverb} {phrase2}."
 
   return sentence.capitalize()
+
+def get_preposition():
+  """Return a randomly chosen preposition
+  from this list of prepositions:
+      "about", "above", "across", "after", "along",
+      "around", "at", "before", "behind", "below",
+      "beyond", "by", "despite", "except", "for",
+      "from", "in", "into", "near", "of",
+      "off", "on", "onto", "out", "over",
+      "past", "to", "under", "with", "without"
+  Return: a randomly chosen preposition.
+  """
+  words = ["about", "above", "across", "after", "along",
+      "around", "at", "before", "behind", "below",
+      "beyond", "by", "despite", "except", "for",
+      "from", "in", "into", "near", "of",
+      "off", "on", "onto", "out", "over",
+      "past", "to", "under", "with", "without"]
+  
+  word = random.choice(words)
+  return word
+
+def get_prepositional_phrase(quantity):
+  """Build and return a prepositional phrase composed
+  of three words: a preposition, a determiner, and a
+  noun by calling the get_preposition, get_determiner,
+  and get_noun functions.
+  Parameter
+      quantity: an integer that determines if the
+          determiner and noun in the prepositional
+          phrase returned from this function should
+          be single or pluaral.
+  Return: a prepositional phrase.
+  """
+  preposition = get_preposition()
+  determiner = get_determiner(quantity)
+  noun = get_noun(quantity)
+
+  # combine into a prepositional phrase
+  phrase = f"{preposition} {determiner} {noun}"
+
+  return phrase
+
+def get_adverb():
+   """Return a randomly chosen adverb."""
+   words = ["quickly", "slowly", "beautifully", "badly",
+        "eagerly", "lazily", "happily", "sadly", "angrily", "calmly"]
+   
+   # Randomly choose and return and adverb
+   word = random.choice(words)
+   return word
 
 main()
